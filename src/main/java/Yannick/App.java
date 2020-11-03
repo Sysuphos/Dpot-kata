9e513baf2cc7a29fd68affa9115d4543b383110e
Sysuphos <yannick.lajdecki@gmail.com>
2020-11-03 22:46:38 +0100

    kata2

--- src/main/java/Yannick/App.java
+++ src/main/java/Yannick/App.java
@@ -1,12 +1,17 @@
 package Yannick;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.sound.sampled.SourceDataLine;
+
 /**
  * Hello world!
  */
 public final class App {
-    private App() {
-    }
 
+    
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
@@ -14,4 +19,92 @@ public final class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+
+    public static boolean isLeapYear(int year){
+        boolean isBissextile = false;
+
+
+		if( ( ((float)year/400) == (Math.floor(year/400)) ) 
+						|| ( ((float)year/4) == (Math.floor(year/4)) 
+						&& ((float)year/100) != (Math.floor(year/100)) ) ){
+			
+			isBissextile = true;	
+			
+        }
+        return isBissextile;
+    }
+
+    public static String compute(int number){
+        
+        String fooBarQix = "";
+        String foo = "Foo";
+        String bar = "Bar";
+        String qix = "Qix";
+
+        //checker Si le chiffre est un modulo de 3 /
+        if(number%3 == 0){
+            fooBarQix = fooBarQix + foo;     
+        }
+        //checker Si le chiffre est un modulo de 5 
+        if(number%5 == 0){
+            fooBarQix = fooBarQix + bar;     
+        }
+        //checker Si le chiffre est un modulo de 7
+        if(number%7 == 0){
+            fooBarQix = fooBarQix + qix;     
+        }
+
+        //go en array
+        List<String> numberString = Arrays.asList(Integer.toString(number).split(""));
+        //  > pour chaque chiffre de l'array de nombres
+        //  > if(chiffe.equals(3))...
+        //  > if(chiffe.equals(5))...
+        //  > etc...
+        for ( String chiffre : numberString) {
+            if(chiffre.equals("3")){
+                fooBarQix += foo;
+            }if(chiffre.equals("5")){
+                fooBarQix += bar;
+            }if(chiffre.equals("7")){
+                fooBarQix += qix;
+            }if(chiffre.equals("0") && !fooBarQix.equals("")){
+                fooBarQix += "*";
+            }
+        }
+        //return result;
+
+        if(fooBarQix.equals("")){
+            for ( String chiffre : numberString) {
+                if(chiffre.equals("0")){
+                    fooBarQix += "*";
+                }else{
+                    fooBarQix += chiffre;
+                }
+            }
+            System.out.println(fooBarQix);
+            return fooBarQix;
+        }
+        System.out.println(fooBarQix);
+        return fooBarQix;
+    }
+
+    public static String writeNumbre(int number){
+        String numberStr = Integer.toString(number);
+        //formate number
+        String formatted = numberStr.replaceAll("(\\d)(?=(\\d{3})+$)", "$1,");
+        
+        //split number
+        String[] parts = formatted.split(",");
+        System.out.println(parts.length);
+        System.out.println(parts[0]);
+
+        String x = "004";
+
+        //String to integer
+        int i = Integer.parseInt(x);  
+        System.out.println(i+6);
+        return parts[0];
+    }
+
 }
--- src/test/java/Yannick/AppTest.java
+++ src/test/java/Yannick/AppTest.java
@@ -15,4 +15,43 @@ class AppTest {
     void testApp() {
         assertEquals(1, 1);
     }
+
+    /**
+	 * donner le dernier jour du mois
+	 */
+	@Test
+	public void isLeapYear() {
+		assertEquals(true, App.isLeapYear(1584));
+		assertEquals(false, App.isLeapYear(1789));
+		assertEquals(false, App.isLeapYear(2003));
+		assertEquals(true, App.isLeapYear(2004));
+		assertEquals(false, App.isLeapYear(2100));
+		assertEquals(true, App.isLeapYear(2216));
+    }
+    
+    @Test
+	public void compute() {
+        assertEquals("1", App.compute(1));
+        assertEquals("FooFoo", App.compute(3));
+        assertEquals("BarBar", App.compute(5));
+        assertEquals("Foo", App.compute(9));
+        assertEquals("FooBarBar", App.compute(15));
+        assertEquals("FooQix", App.compute(21));
+        assertEquals("FooFooFoo", App.compute(33));
+        assertEquals("BarFoo", App.compute(53));
+        assertEquals("1*1", App.compute(101));
+        assertEquals("FooFoo*Foo", App.compute(303));
+        assertEquals("FooBarQix*Bar", App.compute(105));
+        assertEquals("FooQix**", App.compute(10101));
+    }
+    
+    @Test
+	public void writeNumbre() {
+        assertEquals("four thousand three hundred twenty one", App.writeNumbre(4321));
+        assertEquals("fifteen", App.writeNumbre(15));
+        assertEquals("twenty one", App.writeNumbre(21));
+        assertEquals("one hundred and one", App.writeNumbre(101));
+        assertEquals("ten thousand one hundred and one", App.writeNumbre(10101));
+	}
+
 }
